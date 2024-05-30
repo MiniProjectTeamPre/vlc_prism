f335d1edfed78893a3bde1994b9801b74e7a8b0b
AnochaPay <Anocha_ping@hotmail.com>
2024-05-30 15:47:51 +0700

    add

---
+++ vlc_prism/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace vlc_prism {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Index = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Pass = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Prism = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Comment = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PANEL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Index,
+            this.UID,
+            this.Pass,
+            this.Prism,
+            this.Comment,
+            this.PANEL,
+            this.SN});
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle9;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle10.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            dataGridViewCellStyle10.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle10;
+            this.dataGridView1.RowHeadersVisible = false;
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle11;
+            this.dataGridView1.Size = new System.Drawing.Size(1346, 648);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Index
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Index.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Index.HeaderText = "INDEX";
+            this.Index.Name = "Index";
+            this.Index.Width = 70;
+            // 
+            // UID
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.UID.DefaultCellStyle = dataGridViewCellStyle3;
+            this.UID.HeaderText = "UID";
+            this.UID.Name = "UID";
+            this.UID.Width = 200;
+            // 
+            // Pass
+            // 
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Pass.DefaultCellStyle = dataGridViewCellStyle4;
+            this.Pass.HeaderText = "PASS";
+            this.Pass.Name = "Pass";
+            this.Pass.Width = 50;
+            // 
+            // Prism
+            // 
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Prism.DefaultCellStyle = dataGridViewCellStyle5;
+            this.Prism.HeaderText = "PRISM";
+            this.Prism.Name = "Prism";
+            this.Prism.Width = 70;
+            // 
+            // Comment
+            // 
+            this.Comment.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Comment.DefaultCellStyle = dataGridViewCellStyle6;
+            this.Comment.HeaderText = "COMMENT";
+            this.Comment.Name = "Comment";
+            // 
+            // PANEL
+            // 
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.PANEL.DefaultCellStyle = dataGridViewCellStyle7;
+            this.PANEL.HeaderText = "PANEL";
+            this.PANEL.Name = "PANEL";
+            this.PANEL.Width = 200;
+            // 
+            // SN
+            // 
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.SN.DefaultCellStyle = dataGridViewCellStyle8;
+            this.SN.HeaderText = "SN";
+            this.SN.Name = "SN";
+            this.SN.Width = 200;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(164, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "TOTAL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(332, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "LOT_PASS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(515, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "LOT_FAIL";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(955, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "PRISM_FAIL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(753, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "PRISM_PASS";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(1155, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(203, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Save to csv file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 33);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1346, 14);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 750);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Index;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Pass;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Prism;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Comment;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PANEL;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ vlc_prism/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vlc_prism {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private string name_file = "";
+        private string name_pathfile = "";
+        private void button1_Click(object sender, EventArgs e) {
+            OpenFileDialog load = new OpenFileDialog();
+            load.Filter = "csv files (*.csv)|*.csv";
+            load.FilterIndex = 1;
+            DialogResult dd;
+            try {
+                dd = load.ShowDialog();
+            } catch (Exception) {
+                load.InitialDirectory = @"D:\";
+                dd = load.ShowDialog();
+            }
+            if (dd != DialogResult.OK) return;
+            name_file = load.SafeFileName;
+            name_pathfile = load.FileName;
+
+            flag_run = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+        }
+
+        public static void DelaymS(int mS) {
+            Stopwatch stopwatchDelaymS = new Stopwatch();
+            stopwatchDelaymS.Restart();
+            while (mS > stopwatchDelaymS.ElapsedMilliseconds) {
+                if (!stopwatchDelaymS.IsRunning) stopwatchDelaymS.Start();
+                Application.DoEvents();
+            }
+            stopwatchDelaymS.Stop();
+        }
+
+        private bool flag_run = false;
+        private int row = 0;
+        private string index = "";
+        private string uid = "";
+        private string pass = "";
+        private string prism = "";
+        private string comment = "";
+        private string panel = "";
+        private string sn = "";
+        private int total = 0;
+        private int lot_pass = 0;
+        private int lot_fail = 0;
+        private int prism_pass = 0;
+        private int prism_fail = 0;
+        private int row_index = 0;
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e) {
+            while (true) {
+                if (!flag_run) { Thread.Sleep(250); continue; }
+                string[] wo_sup = name_file.Split('_');
+                if (wo_sup.Length != 7) { MessageBox.Show("file err"); return; }
+                string wo = wo_sup[2] + "/" + wo_sup[3];
+                string[] data_csv = File.ReadAllLines(name_pathfile);
+                row_index = data_csv.Length;
+                backgroundWorker1.ReportProgress(1);
+                backgroundWorker1.ReportProgress(10);
+                backgroundWorker1.ReportProgress(12);
+                total = 0;
+                lot_pass = 0;
+                lot_fail = 0;
+                prism_pass = 0;
+                prism_fail = 0;
+                for (int i = 0; i < data_csv.Length; i++) {
+                    row = i;
+                    backgroundWorker1.ReportProgress(11);
+                    string[] data = data_csv[i].Split(',');
+                    if (data[0] == "time_start") continue;
+                    total++;
+                    index = data[2];
+                    uid = data[3].Substring(8, 16);
+                    pass = data[4];
+                    backgroundWorker1.ReportProgress(2);
+                    if (data[4] == "0") {
+                        lot_fail++;
+                        backgroundWorker1.ReportProgress(3);
+                    } else {
+                        lot_pass++;
+                        backgroundWorker1.ReportProgress(4);
+                    }
+                    panel = TeamPrecision.PRISM.cSNs.GetPanel_uid_VLC(wo, uid);
+                    sn = TeamPrecision.PRISM.cSNs.SaveUID_VLC(wo, panel, uid);
+                    backgroundWorker1.ReportProgress(5);
+                    string[] stetus = TeamPrecision.PRISM.cSNs.CheckStatusSNv2(sn, wo);
+                    comment = stetus[1];
+                    if (comment.Contains("ผ่านขั้นตอน FCT1")) {
+                        prism_pass++;
+                        backgroundWorker1.ReportProgress(6);
+                    } else {
+                        prism_fail++;
+                        backgroundWorker1.ReportProgress(7);
+                    }
+                    backgroundWorker1.ReportProgress(8);
+                    backgroundWorker1.ReportProgress(9);
+                    Thread.Sleep(150);
+                }
+                flag_run = false;
+                backgroundWorker1.ReportProgress(0);
+            }
+        }
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e) {
+            switch (e.ProgressPercentage) {
+                case 0:
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                    break;
+                case 1:
+                    dataGridView1.Rows.Clear();
+                    dataGridView1.Rows.Add(row_index);
+                    break;
+                case 2:
+                    dataGridView1.Rows[row].Cells[0].Value = index;
+                    dataGridView1.Rows[row].Cells[1].Value = uid;
+                    dataGridView1.Rows[row].Cells[2].Value = pass;
+                    break;
+                case 3:
+                    dataGridView1.Rows[row].Cells[0].Style.ForeColor = Color.Red;
+                    dataGridView1.Rows[row].Cells[1].Style.ForeColor = Color.Red;
+                    dataGridView1.Rows[row].Cells[2].Style.ForeColor = Color.Red;
+                    break;
+                case 4:
+                    dataGridView1.Rows[row].Cells[0].Style.ForeColor = Color.LimeGreen;
+                    dataGridView1.Rows[row].Cells[1].Style.ForeColor = Color.LimeGreen;
+                    dataGridView1.Rows[row].Cells[2].Style.ForeColor = Color.LimeGreen;
+                    break;
+                case 5:
+                    dataGridView1.Rows[row].Cells[5].Value = panel;
+                    dataGridView1.Rows[row].Cells[6].Value = sn;
+                    break;
+                case 6:
+                    dataGridView1.Rows[row].Cells[3].Value = "PASS";
+                    dataGridView1.Rows[row].Cells[3].Style.ForeColor = Color.LimeGreen;
+                    dataGridView1.Rows[row].Cells[4].Value = comment;
+                    dataGridView1.Rows[row].Cells[4].Style.ForeColor = Color.LimeGreen;
+                    dataGridView1.Rows[row].Cells[6].Style.ForeColor = Color.LimeGreen;
+                    break;
+                case 7:
+                    dataGridView1.Rows[row].Cells[3].Value = "FAIL";
+                    dataGridView1.Rows[row].Cells[3].Style.ForeColor = Color.Red;
+                    dataGridView1.Rows[row].Cells[4].Value = comment;
+                    dataGridView1.Rows[row].Cells[4].Style.ForeColor = Color.Red;
+                    dataGridView1.Rows[row].Cells[6].Style.ForeColor = Color.Red;
+                    break;
+                case 8:
+                    label1.Text = "TOTAL = " + total.ToString();
+                    label2.Text = "LOT_PASS = " + lot_pass.ToString();
+                    label3.Text = "LOT_FAIL = " + lot_fail.ToString();
+                    label5.Text = "PRISM_PASS = " + prism_pass.ToString();
+                    label4.Text = "PRISM_FAIL = " + prism_fail.ToString();
+                    break;
+                case 9:
+                    if (row > 20)
+                        dataGridView1.FirstDisplayedScrollingRowIndex = row - 20;
+                    break;
+                case 10:
+                    progressBar1.Maximum = row_index;
+                    progressBar1.Value = 0;
+                    break;
+                case 11:
+                    progressBar1.Value++;
+                    break;
+                case 12:
+                    this.Text = name_file;
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            //StreamWriter swOut = new StreamWriter(name_file.Replace(".csv", "_log.csv"), true);
+            string[] save = new string[dataGridView1.RowCount];
+            for (int i = 0; i < dataGridView1.RowCount; i++) {
+                //string data = "";
+                //try { data += dataGridView1.Rows[i].Cells[0].Value.ToString() + ","; } catch { }
+                //try { data += dataGridView1.Rows[i].Cells[1].Value.ToString() + ","; } catch { }
+                //try { data += dataGridView1.Rows[i].Cells[2].Value.ToString() + ","; } catch { }
+                //try { data += dataGridView1.Rows[i].Cells[3].Value.ToString() + ","; } catch { }
+                //try { data += dataGridView1.Rows[i].Cells[4].Value.ToString() + ","; } catch { }
+                //try { data += dataGridView1.Rows[i].Cells[5].Value.ToString() + ","; } catch { }
+                //try { data += dataGridView1.Rows[i].Cells[6].Value.ToString() + ","; } catch { }
+                //swOut.WriteLine(data);
+
+
+                try { save[i] += dataGridView1.Rows[i].Cells[0].Value.ToString() + ","; } catch { }
+                try { save[i] += dataGridView1.Rows[i].Cells[1].Value.ToString() + ","; } catch { }
+                try { save[i] += dataGridView1.Rows[i].Cells[2].Value.ToString() + ","; } catch { }
+                try { save[i] += dataGridView1.Rows[i].Cells[3].Value.ToString() + ","; } catch { }
+                try { save[i] += dataGridView1.Rows[i].Cells[4].Value.ToString() + ","; } catch { }
+                try { save[i] = save[i].Replace("ผ่านขั้นตอน FCT1 แล้ว", "Passed the FCT1 procedure"); } catch { }
+                try { save[i] = save[i].Replace("สถานะ", "stetus"); } catch { }
+                try { save[i] += dataGridView1.Rows[i].Cells[5].Value.ToString() + ","; } catch { }
+                try { save[i] += dataGridView1.Rows[i].Cells[6].Value.ToString() + ","; } catch { }
+            }
+            File.WriteAllLines(name_file.Replace(".csv", "_log.csv"), save);
+            MessageBox.Show("Save OK");
+        }
+    }
+}
